bddf3df6636bfa23dff0b805517dab191ee0bf3a
Abdelfattah Abouelaoualim <aabouelaoualim@equancy.com>
2025-09-01 13:20:39 +0200

    Fix nodemailer createTransport method name

--- netlify/functions/stripe-webhook.js
+++ netlify/functions/stripe-webhook.js
@@ -38,7 +38,7 @@ exports.handler = async (event, context) => {
 
   try {
     // Configuration email
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
